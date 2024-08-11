e96acc2d3e35bcd5cfb7afd55d4e0af50356265d
gaetanodigrazia <gaetano.digrazia@bitrock.it>
2024-08-11 22:43:40 +0200

    Initial commit

---
+++ src/main/java/com/digrazia/NoSQLDemo/NoSqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.digrazia.NoSQLDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoSqlDemoApplication.class, args);
+	}
+
+}
